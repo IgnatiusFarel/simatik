dc0c1416b8d966d4b2574e899d62f791b14d7045
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-07-15 22:56:57 +0700

    [UPDATE] - Components - Dashboard - View

--- frontend/src/components/dashboard/DashboardView.vue
+++ frontend/src/components/dashboard/DashboardView.vue
@@ -1,10 +1,40 @@
 <template>
-  <h1> Hai </h1>
+  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+    <AssetCard
+      total="1.234"
+      title="Semua Aset"
+      subTitle="Aset TIK"
+      subtitleDesc="Semua Aset"
+      percentage="100%"    
+      :isCollapsed="isSidebarCollapsed"
+    />
+    <AssetCard
+      total="534"
+      title="Aset Digunakan"
+      subTitle="Aset TIK"
+      subtitleDesc="Digunakan"
+      percentage="43%"      
+       :isCollapsed="isSidebarCollapsed"
+    />
+    <AssetCard
+      total="700"
+      title="Aset Cadangan"
+      subTitle="Aset TIK"
+      subtitleDesc="Cadangan"
+      percentage="57%"      
+       :isCollapsed="isSidebarCollapsed"
+    />
+  </div>
 </template>
 
 <script>
-export default {
+import AssetCard from './AssetCard.vue'
 
+export default {
+  name: 'Dashboard',
+  components: {
+    AssetCard
+  }
 }
 </script>
 
