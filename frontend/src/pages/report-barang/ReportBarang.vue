7e739e571d8a57107214c233fc8bedaa8c3f6718
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-07-16 23:34:05 +0700

    [ADD] - Pages - Report Brang

---
+++ frontend/src/pages/report-barang/ReportBarang.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useHead } from '@vueuse/head';
+
+useHead({
+  title: 'Halaman Report Barang - SIMATIK',
+  meta: [
+    {
+      name: 'description',
+      content:
+        'Sistem Informasi Manajemen Aset TIK',
+    },
+  ],
+});
+</script>
+
+<template>    
+  <h1> Ini adalah report barang</h1>  
+</template>
