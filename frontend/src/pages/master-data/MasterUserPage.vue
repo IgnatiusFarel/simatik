3d09739c892e911d306189e71380702f85900c40
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-07-16 23:33:52 +0700

    [ADD] - Pages - Master Data - Master User Page

---
+++ frontend/src/pages/master-data/MasterUserPage.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useHead } from '@vueuse/head';
+
+useHead({
+  title: 'Halaman Master User - SIMATIK',
+  meta: [
+    {
+      name: 'description',
+      content:
+        'Sistem Informasi Manajemen Aset TIK',
+    },
+  ],
+});
+</script>
+
+<template>    
+  <h1> Ini adalah master user wak</h1>  
+</template>
