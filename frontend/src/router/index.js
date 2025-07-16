2aefcd5529ea0b89a1ffd36eb13f5d320d2d4e73
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-07-16 23:34:24 +0700

    [UPDATE] - Router - Index.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import LoginPage from '@/pages/auth/LoginPage.vue';
 import DashboardPage from '../pages/dashboard/DashboardPage.vue';
+import MasterUserPage from '../pages/master-data/MasterUserPage.vue';
+import MasterBarangPage from '../pages/master-data/MasterBarangPage.vue'
+import ReportBarangPage from '../pages/report-barang/ReportBarang.vue';
 
 const routes = [
-  { path: '/login', component: LoginPage, meta: {public: true}},  
+  { path: '/login', component: LoginPage, meta: { public: true, layout: 'empty' } },
   { path: '/dashboard', component: DashboardPage, meta: {public: true}},  
+  { path: '/master-barang', component: MasterBarangPage, meta: {public: true}},  
+  { path: '/master-user', component: MasterUserPage, meta: {public: true}},  
+  { path: '/report-barang', component: ReportBarangPage, meta: {public: true}},  
 ];
 
 const router = createRouter({
