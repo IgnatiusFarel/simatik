dbe2e44dc17f51b2c4ba15e7e2444e8f57955365
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-07-16 23:31:36 +0700

    [UPDATE] - App

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,18 @@
 <template>
-<router-view />
+  <component :is="layoutComponent">
+    <router-view />
+  </component>
 </template>
 
 <script setup>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import DashboardLayout from './components/layout/DashboardLayout.vue'
+import EmptyLayout from './components/layout/EmptyLayout.vue'
 
+const route = useRoute()
+
+const layoutComponent = computed(() => {
+  return route.meta.layout === 'empty' ? EmptyLayout : DashboardLayout
+})
 </script>
-
-<style scoped>
-</style>
