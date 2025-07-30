820b8fd336034f8ca1313d659aed4adac898f618
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-07-30 23:37:24 +0700

    [ADD] - Components - Report Barang - View

---
+++ frontend/src/components/report-barang/ReportBarangView.vue
@@ -0,0 +1,66 @@
+<template>
+   <CustomTable :columns="columns" :data="data">
+    <template #header-filter>
+      <a-range-picker v-model:value="value1" />
+    </template>
+    <template #header-action>
+      <a-button
+        class="flex items-center gap-2 bg-[#d30007] text-white px-3 py-1 rounded-[8px] font-semibold"
+      >
+        <Plus class="w-4 h-4" /> Print PDF</a-button
+      >
+    </template>  
+    </CustomTable>
+   
+</template>
+
+<script setup>
+import CustomTable from '../CustomTable.vue';
+import { Plus } from "lucide-vue-next";
+
+const data = [
+  {
+    key: "1",
+    seri: "#20462",
+    barang: "Hat",
+    tahun_pengadaan: 2014,
+    pemeliharaan: "13/05/2022",
+    harga_barang: "Rp.13.000.00",
+    kategori: "Komputer",
+    status: "Baik",
+  },
+  {
+    key: "1",
+    seri: "#20462",
+    barang: "Hat",
+    tahun_pengadaan: 2014,
+    pemeliharaan: "13/05/2022",
+    harga_barang: "Rp.13.000.00",
+    kategori: "Komputer",
+    status: "Baik",
+  },
+  {
+    key: "1",
+    seri: "#20462",
+    barang: "Hat",
+    tahun_pengadaan: 2014,
+    pemeliharaan: "13/05/2022",
+    harga_barang: "Rp.13.000.00",
+    kategori: "Komputer",
+    status: "Baik",
+  },  
+]
+
+const columns = [
+  { title: "No. Seri", dataIndex: "seri" },
+  { title: "Barang", dataIndex: "barang" },
+  { title: "Tahun Pengadaan", dataIndex: "tahun_pengadaan" },
+  { title: "Pemeliharaan", dataIndex: "pemeliharaan" },
+  { title: "Harga Barang", dataIndex: "harga_barang" },
+  { title: "Kategori", dataIndex: "kategori" },
+  { title: "Status", dataIndex: "status" },  
+]
+</script>
+
+<style>
+</style>
