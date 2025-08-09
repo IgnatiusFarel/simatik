48ddc7244e3da970b97c5e1ba611808c62e43efb
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-08-09 11:27:05 +0700

    [UPDATE] - Components - Custom Table

--- frontend/src/components/CustomTable.vue
+++ frontend/src/components/CustomTable.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-white shadow-sm p-4 rounded-md">
-    <div class="flex items-center justify-between mb-4 flex-wrap gap-2">
+    <div class="flex items-center justify-between mb-4 flex-wrap gap-4">
       <div class="flex items-center gap-4 flex-wrap">
         <div class="flex items-center gap-2">
           <label class="text-sm font-medium">Show</label>
