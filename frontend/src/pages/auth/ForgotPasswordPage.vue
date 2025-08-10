ff8d9329b5dcce3fd8f565f170465464e87de1ba
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-08-10 22:16:10 +0700

    [ADD] - Pages - Auth - Forgot Password Page

---
+++ frontend/src/pages/auth/ForgotPasswordPage.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { useHead } from '@vueuse/head';
+import ForgotPasswordView from '@/components/auth/ForgotPasswordView.vue';
+
+useHead({
+  title: 'Halaman Forgot Password - SIMATIK',
+   meta: [
+    {
+      name: 'description',
+      content: 'Atur ulang kata sandi Anda untuk dapat kembali mengakses sistem informasi manajemen aset TIK SIMATIK.',
+    },
+    {
+      name: 'keywords',
+      content: 'dashboard aset, SIMATIK, sistem informasi aset, manajemen aset TIK, monitoring aset',
+    },
+    {
+      name: 'author',
+      content: 'Practice My Full-Stack Skill',
+    },
+    {
+      property: 'og:title',
+      content: 'Dashboard SIMATIK',
+    },
+    {
+      property: 'og:description',
+      content: 'Pantau aset teknologi informasi dan komunikasi Anda dari satu tempat dengan dashboard SIMATIK.',
+    },
+    {
+      property: 'og:type',
+      content: 'website',
+    },
+    {
+      charset: 'utf-8',
+    },
+  ],
+  link: [
+    {
+      rel: 'icon',
+      href: '/Logo.png',
+    },
+  ],
+});
+</script>
+
+<template>
+  <ForgotPasswordView />
+</template>
