f13d82ddb74ca5f1972e5ca13a19b4b6e439fde7
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-08-10 22:16:25 +0700

    [UPDATE] - Pages - Auth - Login Page

--- frontend/src/pages/auth/LoginPage.vue
+++ frontend/src/pages/auth/LoginPage.vue
@@ -4,11 +4,39 @@ import LoginView from '@/components/auth/LoginView.vue';
 
 useHead({
   title: 'Halaman Login - SIMATIK',
-  meta: [
+   meta: [
     {
       name: 'description',
-      content:
-        'Sistem Informasi Manajemen Aset TIK',
+      content: 'Masuk ke SIMATIK untuk mengelola dan memantau aset TIK organisasi Anda secara real-time.',
+    },
+    {
+      name: 'keywords',
+      content: 'dashboard aset, SIMATIK, sistem informasi aset, manajemen aset TIK, monitoring aset',
+    },
+    {
+      name: 'author',
+      content: 'Practice My Full-Stack Skill',
+    },
+    {
+      property: 'og:title',
+      content: 'Dashboard SIMATIK',
+    },
+    {
+      property: 'og:description',
+      content: 'Pantau aset teknologi informasi dan komunikasi Anda dari satu tempat dengan dashboard SIMATIK.',
+    },
+    {
+      property: 'og:type',
+      content: 'website',
+    },
+    {
+      charset: 'utf-8',
+    },
+  ],
+  link: [
+    {
+      rel: 'icon',
+      href: '/Logo.png',
     },
   ],
 });
