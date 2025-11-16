ef984813c1a188d337294433ce60c05fcef288aa
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-11-16 14:28:33 +0700

    [UPDATE] - Index php

--- backend/public/index.php
+++ backend/public/index.php
@@ -5,16 +5,16 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
+// maintenance mode
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
+// composer autoload
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
+// bootstrap Laravel
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
+// handle request
 $app->handleRequest(Request::capture());
