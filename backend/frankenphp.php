ce9823669b95b18b87785fd553864f47a1381494
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-11-16 14:02:21 +0700

    [UPDATE] - Frankenphp

--- backend/frankenphp.php
+++ backend/frankenphp.php
@@ -1,20 +1,10 @@
 <?php
 
-use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
-
-define('LARAVEL_START', microtime(true));
-
-// maintenance mode
-if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-// composer autoload
-require __DIR__.'/vendor/autoload.php';
-
-// bootstrap Laravel
-$app = require_once __DIR__.'/bootstrap/app.php';
-
-// handle request
-$app->handleRequest(Request::capture());
+return [
+    'workers' => [
+        [
+            'type' => 'php',
+            'script' => 'public/index.php',
+        ],
+    ],
+];
