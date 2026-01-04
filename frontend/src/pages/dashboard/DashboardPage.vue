e4f493341f2fe59896c3864381be4473df15bb74
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2026-01-04 17:15:46 +0700

    [UPDATE] - Pages - Dashboard

--- frontend/src/pages/dashboard/DashboardPage.vue
+++ frontend/src/pages/dashboard/DashboardPage.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { useHead } from '@vueuse/head';
-import DashboardView from '../../components/dashboard/DashboardView.vue';
+import DashboardView from '@/components/dashboard/DashboardView.vue';
 
 useHead({
   title: 'Halaman Dashboard - SIMATIK',
