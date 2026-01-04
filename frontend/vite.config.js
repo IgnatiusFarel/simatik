020c162ab4f3ccab3b1f8432d6f1362fc3d24aac
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2026-01-04 17:13:49 +0700

    [UPDATE] - Configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,11 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import Components from 'unplugin-vue-components/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [
-    vue(),
+    vue(), tsconfigPaths(),
     Components({
       resolvers: [
         AntDesignVueResolver({
@@ -20,4 +21,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  test: {
+    environment: 'jsdom', 
+    globals: true,
+      setupFiles: "./test/setup.js",
+  }
 })
